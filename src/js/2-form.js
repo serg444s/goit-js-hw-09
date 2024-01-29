b8998d8dd1fad15fa89b1cb547bda3bd587a4f54
serg444s <143083342+serg444s@users.noreply.github.com>
2024-01-29 15:12:56 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,29 +1,31 @@
 const userForm = document.querySelector('.feedback-form');
-const localStorageKey = 'feedback-form-state';
 const userTextarea = userForm.elements.message;
 const userEmail = userForm.elements.email;
+const localStorageKey = 'feedback-form-state';
 
 userForm.addEventListener('input', onFormInput);
+userForm.addEventListener('submit', onFormSubmit);
 
-function onFormInput(event) {
+function onFormInput() {
+  const email = userForm.elements.email.value.trim();
+  const message = userForm.elements.message.value.trim();
   const userFormData = {
-    email: userEmail.value.trim(),
-    message: userTextarea.value.trim(),
+    email,
+    message,
   };
   localStorage.setItem(localStorageKey, JSON.stringify(userFormData));
 }
 
-const userFormDataParse = JSON.parse(localStorage.getItem(localStorageKey));
-if (userFormDataParse) {
-  userEmail.value = userFormDataParse.email ?? '';
-  userTextarea.value = userFormDataParse.message ?? '';
+const returnedFormData = localStorage.getItem(localStorageKey);
+if (returnedFormData) {
+  const parsedForm = JSON.parse(returnedFormData);
+  userEmail.value = parsedForm.email;
+  userTextarea.value = parsedForm.message;
 }
-
-userForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  if (userTextarea.value && userEmail.value) {
+  if (userTextarea.value.trim() && userEmail.value.trim()) {
     console.log(JSON.parse(localStorage.getItem(localStorageKey)));
     localStorage.removeItem(localStorageKey);
     userForm.reset();
